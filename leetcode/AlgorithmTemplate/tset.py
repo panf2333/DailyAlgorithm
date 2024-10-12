442eaa3340811482cc49ef3d6fad67d7333de775
clark <panf2333@gmail.com>
2024-10-12 10:17:09 +0800

    20241012_3158. Find the XOR of Numbers Which Appear Twice

--- leetcode/AlgorithmTemplate/tset.py
+++ leetcode/AlgorithmTemplate/tset.py
@@ -1,44 +1,26 @@
-from collections import defaultdict
-class Solution:
-    def goodSubsetofBinaryMatrix(self, grid: list[list[int]]) -> list[int]:
-        memo = defaultdict(list)
-        n = len(grid)
-        m = len(grid[0])
+import os
+import openai
+import httpx
 
-        for i, row in enumerate(grid):
-            num: int = 0
-            base: int = 1
-            for val in row:
-                if val == 1:
-                    num |= base
-                base <<= 1
-            memo[num].append(i)
-        
-        if (len(memo[0]) > 0):
-            return sorted(memo[0])
-        uplimit = (1 << m) - 1
-        print(uplimit)
-        print(memo)
-        for i in range(1, 1 << m):
-            if len(memo[i]) == 0:
-                continue
-            target = uplimit ^ i
-            sub = target
-            while sub:
-                # 处理 sub 的逻辑
-                if len(memo[sub]) == 0:
-                    sub = (sub - 1) & target
-                    continue
-                # ans = [memo[i][0], memo[sub][0]]
-                # ans.sort()
-                # return ans
-                return sorted([memo[i][0], memo[sub][0]])
-        return []
-            
-# 1001
+system_content = "You are a gourmet. Be descriptive and helpful."
+user_content = "Tell me about Chinese hotpot"
 
-a = Solution()
-# grid = [[0,1,1,0],[0,0,0,1],[1,1,1,1]]
-# grid = [[0,0],[1,1],[1,0],[1,0]]
-grid = [[0,0,0,1],[0,1,1,0]]
-print(a.goodSubsetofBinaryMatrix(grid))
+http_client = httpx.Client(verify=False)
+client = openai.OpenAI(
+    http_client = http_client,
+    api_key="API_KEY",
+    base_url="https://yotta-llvm-1.yottadeos.com/v1",
+    )
+
+chat_completion = client.chat.completions.create(
+    model="meta-llama/Llama-3.2-3B-Instruct",
+    messages=[
+        {"role": "system", "content": system_content},
+        {"role": "user", "content": user_content},
+    ],
+    temperature=0.7,
+    max_tokens=100,
+)
+print("Response:\n", chat_completion)
+response = chat_completion.choices[0].message.content
+print("Response:\n", response)
